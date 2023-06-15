dd34392bb9b102a2a576b3932ac40e601106a900
Alexander Podkopaev <alexander@podkopaev.net>
2023-06-15 22:05:43 +0300

    Added movement implementation

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,33 +1,138 @@
-using System;
-using UnityEditor.UIElements;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
-public class PlayerMovement : MonoBehaviour {
-    public float speed = 8f;
-    public float jump = 8f;
-    public float gravity = 8f;
+// Code from: https://github.com/Vidsneezes/2dKinematicControllerUnity
 
-    private float MoveHor;
-    private float MoveVer;
+ public class PlayerMovement : MonoBehaviour {
 
-    public Rigidbody2D rb;
+    public bool isRightPressed;
+    public bool isLeftPressed;
+    public bool wasJumpPressed;
 
-    void Update() {
-        MoveHor = Input.GetAxisRaw("Horizontal");
-        Vector2 newRb = new Vector2(MoveHor * speed, rb.velocity.y);
+    public float moveSpeed;
+    public float gravity;
+    public float startJumpSpeed;
+    public LayerMask groundMask;
+    [HideInInspector]
+    public BoxCollider2D hitBox;
+    [HideInInspector]
+    public SpriteRenderer mainSprite;
+    public bool onGround;
 
-        if (Input.GetButtonDown("Jump")) {
-            newRb.y += jump;
+
+    public Vector2 velocity;
+    private Vector2 direction;
+    [HideInInspector]
+    public Rigidbody2D rBody2d;
+    private float actualMoveSpeed;
+    [HideInInspector]
+    public GameObject playerSpawnPoint;
+
+    private void Awake()
+    {
+        rBody2d = GetComponent<Rigidbody2D>();
+        mainSprite = GetComponent<SpriteRenderer>();
+        hitBox = GetComponent<BoxCollider2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        direction = Vector2.right;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        isRightPressed = Input.GetKey(KeyCode.D);
+        isLeftPressed = Input.GetKey(KeyCode.A);
+        wasJumpPressed = Input.GetKeyDown(KeyCode.Space);
+
+
+        if (isRightPressed)
+        {
+            direction.x = 1;
+            mainSprite.flipX = false;
+            actualMoveSpeed = moveSpeed;
+        }else if (isLeftPressed)
+        {
+            direction.x = -1;
+            mainSprite.flipX = true;
+            actualMoveSpeed = moveSpeed;
+        }
+        else
+        {
+            actualMoveSpeed = 0;
         }
 
-        newRb.y -= gravity * Time.deltaTime;
-
-        RaycastHit2D hit = Physics2D.BoxCast(transform.position, new Vector2(1, 2), 0, newRb * Time.deltaTime,
-            newRb.magnitude * Time.deltaTime);
 
 
+        if (wasJumpPressed && onGround)
+        {
+            velocity.y = startJumpSpeed;
+            onGround = false;
+        }
 
-        rb.velocity = newRb;
     }
+
+    private void FixedUpdate()
+    {
+        Vector2 currentPosition = rBody2d.position;
+        velocity.y -= gravity * Time.deltaTime;
+
+
+        //horizontal check
+        velocity.x = direction.x * actualMoveSpeed;
+        float final_hor_vel = velocity.x;
+
+        if (PlaceMeet(currentPosition.x + final_hor_vel * Time.deltaTime, currentPosition.y, groundMask))
+        {
+            while (!PlaceMeet(currentPosition.x + ((final_hor_vel * Time.deltaTime) / 8), currentPosition.y, groundMask))
+            {
+                currentPosition.x += ((final_hor_vel * Time.deltaTime) / 8);
+            }
+            final_hor_vel = 0;
+        }
+        currentPosition.x += final_hor_vel * Time.deltaTime;
+
+
+        //vertical check
+        float final_ver_vel = velocity.y;
+        if (PlaceMeet(currentPosition.x, currentPosition.y + final_ver_vel * Time.deltaTime, groundMask))
+        {
+            while (!PlaceMeet(currentPosition.x, currentPosition.y + ((final_ver_vel * Time.deltaTime) / 8), groundMask))
+            {
+                currentPosition.y += ((final_ver_vel * Time.deltaTime) / 8);
+            }
+            final_ver_vel = 0;
+        }
+        currentPosition.y += final_ver_vel * Time.deltaTime;
+
+        rBody2d.MovePosition(currentPosition);
+
+        if (PlaceMeet(currentPosition.x, currentPosition.y + (1f / 8f), groundMask))
+        {
+            velocity.y = 0;
+        }
+
+        if (PlaceMeet(currentPosition.x, currentPosition.y - (1f / 8f),groundMask))
+        {
+            velocity.y = 0;
+            onGround = true;
+        }
+        else
+        {
+            onGround = false;
+        }
+
+
+    }
+
+    private bool PlaceMeet(float xPos, float yPos, LayerMask mask)
+    {
+        Vector2 position = new Vector2(xPos+hitBox.offset.x, yPos+hitBox.offset.y);
+        Collider2D[] collider2d = Physics2D.OverlapBoxAll(position , hitBox.size, 0, mask);
+        return collider2d.Length > 0;
+    }
+
 }
