82ca7c48dfd37f6d62c4854e0e764582f68b1558
svyatslava233 <harik10346@gmail.com>
2023-06-28 23:28:36 +0300

    Venus base implementation

--- Assets/Scripts/ZombieSystem/ZombieBehaviour.cs
+++ Assets/Scripts/ZombieSystem/ZombieBehaviour.cs
@@ -13,7 +13,7 @@ public class ZombieBehaviour : CursedBehaviour {
     private Rigidbody2D rigidBody2D;
 
     // Start is called before the first frame update
-    void Start() {
+    protected override void ExtraAwake() {
         rigidBody2D = GetComponent<Rigidbody2D>();
     }
 
